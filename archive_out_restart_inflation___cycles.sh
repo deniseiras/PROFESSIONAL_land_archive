778012514b9940265b03b41372fe2bfc748df7eb
deniseiras <denis.eiras@gmail.com>
2025-07-01 14:53:30 +0200

    One year by time

--- archive_out_restart_inflation___cycles.sh
+++ archive_out_restart_inflation___cycles.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Define parameters
-exp_name="d4o_all30_v4"
+exp_name="d4o_all30_v8"
 dry_run=true
 rm_files=true
-start_year=2005
-end_year=2006
+year=2014
 start_month=01
-end_month=12  
+end_month=12
 
 # Iterate over months from start_month to end_month
 for month in $(seq -w $start_month $end_month); do
@@ -19,8 +18,8 @@ for month in $(seq -w $start_month $end_month); do
     esac
     
     # Execute the command for the current month
-    echo "Executing ""$exp_name" "$dry_run" "$rm_files" "$start_year" "$end_year" "$month" "$month" 01 "$days"
-    ./archive_out_restart_inflation.sh "$exp_name" "$dry_run" "$rm_files" "$start_year" "$end_year" "$month" "$month" 01 "$days" &
+    echo "Executing ""$exp_name" "$dry_run" "$rm_files" "$year" "$year" "$month" "$month" 01 "$days"
+    ./archive_out_restart_inflation.sh "$exp_name" "$dry_run" "$rm_files" "$year" "$year" "$month" "$month" 01 "$days" &
     
     # Optional: Wait for the command to finish before proceeding
     wait
